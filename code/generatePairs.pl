131a56a96204a00a1819a0e4ffd8a360f8228e45
KBC <kevin.cohen@gmail.com>
2021-06-01 13:40:55 -0500

    There is a bug that causes both terms in the opposites pair to be identical...

--- code/generatePairs.pl
+++ code/generatePairs.pl
@@ -5,6 +5,8 @@
 
 use strict 'vars';
 
+my $DEBUG = 0;
+
 #my %increased = ();
 #my %decreased = ();
 
@@ -20,33 +22,30 @@ my @decrease = ("decreased",
                 "decrease",
                 "decreases");
 
+my @hyper = ("hyper");
+my @hypo = ("hypo");
+
+if (0) {
+  @increase = @hyper;
+  @decrease = @hypo;
+}
+
 while (my $line = <>) {
 
   # a little preprocessing
   chomp($line);
-  #$line =~ s/^name:\s+//;
+  $line =~ s/^name:\s+//;
 
   generateCrossProduct("OPPOSITE", $line, \@increase, \@decrease);
-  generateCrossProduct("SYNONYM", $line, \@increase, \@increase);
-  generateCrossProduct("SYNONYM", $line, \@decrease, \@decrease);
+  #generateCrossProduct("SYNONYM", $line, \@increase, \@increase);
+  #generateCrossProduct("SYNONYM", $line, \@decrease, \@decrease);
 
   #if ($line =~ s/increased/_/gi) { $increased{$line}++ }
   #if ($line =~ s/decreased/_/gi) { $decreased{$line}++ }
-  my @increase_synset = ();
-  my @decrease_synste = ();
+  #my @increase_synset = ();
+  #my @decrease_synset = ();
 
-  #foreach my $increase_word (@increase) {
-  #  my $output = $line;
-  #  $output =~ s/_/$increase_word/;
-  #  print "$output\n";
-  #}
-  #foreach my $decrease_word (@decrease) {
-  #  my $output = $line;
-  #  $output =~ s/_/$decrease_word/;
-  #  print "$output\n";
-  #} 
 
-  generateCrossProduct(\@increase, \@decrease, $line);
 }
 
 
@@ -63,18 +62,24 @@ sub generateCrossProduct {
   # oh, fuck--how do you pass in two arrays?? Better do two
   # array references and then dereference them, you dumb fuck, Kevin
   my ($label, $term, $array_referenceX, $array_referenceY) = @_;
-  foreach my $X_element (@{$array_referenceX}) {
+  1 && print "Label: $label Term: $term Array ref X: $array_referenceX Array ref Y: $array_referenceY\n";
 
-    my $outputX = $term;
+  my $outputX = $term;
+  my $outputY = $term;
+
+  OUTER: foreach my $X_element (@{$array_referenceX}) {
+  $DEBUG && print "Outer loop, X_element is $X_element\n";
     $outputX =~ s/_/$X_element/;
-    foreach my $Y_element (@{$array_referenceY}) {
-      # little hack for the case where synonyms are actually identical
-      if ($Y_element eq $X_element) { next; }
+    $DEBUG && print "output for X: $outputX\n";
 
-      my $outputY = $term;
+    INNER: foreach my $Y_element (@{$array_referenceY}) {
+    1 && print "Inner loop, Y_element is $Y_element\n";
+
       $outputY =~ s/_/$Y_element/;
-      #print "$X_element $term\t$Y_element $term\n";
+      $DEBUG && print "output for Y: $outputY\n";
+      # little hack for the case where synonyms are actually identical
+      if ($Y_element eq $X_element) { $DEBUG && print "IDENTICAL: $Y_element $X_element\n"; last OUTER; }
       print "$label\t$outputX\t$outputY\n"; 
     }
   }
-}
+} # close function definition generateCrossProduct()
